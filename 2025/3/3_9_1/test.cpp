6734a1d2eb3912749d4774b7fbfebf12031514cf
HCYshawn <1754099901@qq.com>
2025-03-09 20:36:15 +0800

    输出流错误流定向到不同文件

---
+++ 2025/3/3_9_1/test.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello cout"<<endl;
+    printf("Hello printf\n");
+
+    cerr<<"Hello cerr"<<endl;
+    fprintf(stderr,"Hello stderr\n");
+    return 0;
+}
