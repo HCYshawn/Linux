bb7c5db74e71467685d41e7e07cb144ea4943f0f
HCYshawn <1754099901@qq.com>
2025-05-28 21:12:27 +0800

    日志系统(10) 异步缓冲区类实现

--- project/log/logs/buffer.hpp
+++ project/log/logs/buffer.hpp
@@ -1,59 +1,98 @@
 #include <vector>
+#include <cassert>
 #include "util.hpp"
 
 namespace bitlog
 {
 #define DEFAULT_BUFFER_SIZE (100 * 1024 * 1024)
-
+#define THRESHOLD_BUFFER_SIZE (80 * 1024 * 1024)
+#define INCREMENT_BUFFER_SIZE (10 * 1024 * 1024)
     class Buffer
     {
     public:
-        Buffer()
+        Buffer() : _buffer(DEFAULT_BUFFER_SIZE), _writer_idx(0), _reader_idx(0)
         {
         }
 
         // 向缓冲区写入数据
         void push(const char *data, size_t len)
         {
+            // 检测空间大小
+            ensureEnoughSize(len);
+            // 拷贝至缓冲区
+            std::copy(data, data + len, &_buffer[_writer_idx]);
+            // 写入位置向后偏移
+            moveWriter(len);
+        }
+
+        size_t writeAbleSize()
+        {
+            return (_buffer.size() - _writer_idx);
         }
 
         // 返回可读数据的地址
         const char *begin()
         {
+            return &_buffer[_writer_idx];
         }
 
         // 返回可读数据的长度
         size_t readAbleSize()
         {
+            return (_writer_idx - _reader_idx);
         }
 
         // 对读写指针进行向后偏移操作
-        void moveWriter(size_t len)
-        {
-        }
-
         void moveReader(size_t len)
         {
+            assert(len <= readAbleSize());
+            _reader_idx += len;
         }
 
         // 重置读写位置，初始化缓冲区
         void reset()
         {
+            _writer_idx = 0;
+            _reader_idx = 0;
         }
 
         // 实现交换操作
-        void swap(const Buffer &buffer)
+        void swap(Buffer &buffer)
         {
+            _buffer.swap(buffer._buffer);
+            std::swap(_reader_idx, buffer._reader_idx);
+            std::swap(_writer_idx, buffer._writer_idx);
         }
 
         // 判断缓冲区是否为空
         bool empty()
         {
+            return (_reader_idx == _writer_idx);
         }
 
     private:
+        // 对空间进行扩容
+        void ensureEnoughSize(size_t len)
+        {
+            if (len < writeAbleSize())
+                return; // 不需要扩容
+            size_t new_size = 0;
+            if (_buffer.size() < THRESHOLD_BUFFER_SIZE)
+            {
+                new_size = _buffer.size() * 2; // 翻倍增长
+            }
+            else
+            {
+                new_size = _buffer.size() + INCREMENT_BUFFER_SIZE; // 线性增长
+            }
+            _buffer.resize(new_size);
+        }
+
+        // 对读写指针进行向后偏移操作
         void moveWriter(size_t len)
         {
+            assert((len + _writer_idx) <= _buffer.size());
+            _writer_idx += len;
         }
 
     private:
