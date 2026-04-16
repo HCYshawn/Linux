910d173f891c42de8d20c82c05d5eb3c77ef6b8a
HCYshawn <1754099901@qq.com>
2026-04-16 16:15:04 +0800

    cmake test2

---
+++ 2026/4m/4_16_1/cmake_tools/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/my/local/2026/4m/4_16_1/cmake_tools
+# Build directory: /root/my/local/2026/4m/4_16_1/cmake_tools/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(testAdd "/root/my/local/2026/4m/4_16_1/cmake_tools/build/main")
+set_tests_properties(testAdd PROPERTIES  _BACKTRACE_TRIPLES "/root/my/local/2026/4m/4_16_1/cmake_tools/CMakeLists.txt;12;add_test;/root/my/local/2026/4m/4_16_1/cmake_tools/CMakeLists.txt;0;")
