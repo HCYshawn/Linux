835c65bd5525619e0f59b02283e4d74c27727185
HCYshawn <1754099901@qq.com>
2026-05-17 15:40:21 +0800

    cmake 静态库测试

---
+++ 2026/5m/5_17_1/build/my_lib/CMakeFiles/MyMath.dir/src/add.cpp.o.d
@@ -0,0 +1,4 @@
+my_lib/CMakeFiles/MyMath.dir/src/add.cpp.o: \
+ /root/my/local/2026/5m/5_17_1/my_lib/src/add.cpp \
+ /usr/include/stdc-predef.h \
+ /root/my/local/2026/5m/5_17_1/my_lib/include/math.h
---
+++ 2026/5m/5_17_1/build/my_lib/CMakeFiles/MyMath.dir/src/sub.cpp.o.d
@@ -0,0 +1,4 @@
+my_lib/CMakeFiles/MyMath.dir/src/sub.cpp.o: \
+ /root/my/local/2026/5m/5_17_1/my_lib/src/sub.cpp \
+ /usr/include/stdc-predef.h \
+ /root/my/local/2026/5m/5_17_1/my_lib/include/math.h
