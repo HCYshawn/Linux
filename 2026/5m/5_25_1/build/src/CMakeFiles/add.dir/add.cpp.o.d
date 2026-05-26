30abddeff4689cbbd900a924dd2e8b0a963b5721
HCYshawn <1754099901@qq.com>
2026-05-26 16:30:35 +0800

    cmake properties测试

---
+++ 2026/5m/5_25_1/build/src/CMakeFiles/add.dir/add.cpp.o.d
@@ -0,0 +1,2 @@
+src/CMakeFiles/add.dir/add.cpp.o: \
+ /root/my/local/2026/5m/5_25_1/src/add.cpp /usr/include/stdc-predef.h
---
+++ 2026/5m/5_25_1/build/src/CMakeFiles/add.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../bin/.20"
+  "../bin/libadd.pdb"
+  "../bin/libadd.so"
+  "../bin/libadd.so.1.2.3"
+  "../bin/libadd.so.20"
+  "CMakeFiles/add.dir/add.cpp.o"
+  "CMakeFiles/add.dir/add.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
