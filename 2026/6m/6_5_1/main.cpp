edbd294ba7f89b686f1564b34fb1935257d1efa5
HCYshawn <1754099901@qq.com>
2026-06-06 10:04:43 +0800

    test

--- 2026/6m/6_5_1/main.cpp
+++ 2026/6m/6_5_1/main.cpp
@@ -4,6 +4,7 @@
 int main() {
   std::vector<int> a = {1, 2, 3};
   a.push_back(1);
+  std::cout << "test" << std::endl;
   for (int i = 0; i < 4; i++) {
     std::cout << a[i] << " " << std::endl;
   }
