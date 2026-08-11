d70616c33be2f88304dbe1f37e19e4def1ab32c2
HCYshawn <1754099901@qq.com>
2026-08-11 10:44:31 +0800

    进程信号捕获测试

---
+++ 2026/8m/8_11_1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
+
+void handlerSig(int sig) {
+  std::cout << "获得了一个信号: " << sig << std::endl;
+}
+
+int main() {
+  signal(SIGINT, handlerSig);
+  int cnt = 0;
+  while (true) {
+    std::cout << "cnt: " << cnt++ << std::endl;
+    sleep(1);
+  }
+  return 0;
+}
