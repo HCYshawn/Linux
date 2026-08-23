76edd0875890d5ad3e40be56e46a014705f802ce
HCYshawn <1754099901@qq.com>
2026-08-23 19:38:43 +0800

    打印线程id

---
+++ 2026/8m/8_23_3/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <unistd.h>
+#include <pthread.h>
+
+void showtid(pthread_t &tid)
+{
+    printf("tid: 0x%lx\n", tid);
+}
+
+std::string FormatId(const pthread_t &tid)
+{
+    char id[64];
+    snprintf(id, sizeof(id), "0x%lx", tid);
+    return id;
+}
+
+void *routine(void *args)
+{
+    std::string name = static_cast<const char *>(args);
+    pthread_t tid = pthread_self();
+    int cnt = 5;
+    while (cnt)
+    {
+        std::cout << "我是一个新线程: name: " << name
+                  << "我的id: " << FormatId(tid) << std::endl;
+        sleep(1);
+        cnt--;
+    }
+    return nullptr;
+}
+
+int main()
+{
+    pthread_t tid;
+    int n = pthread_create(&tid, nullptr, routine, (void *)"thread-1");
+    (void)n;
+
+    showtid(tid);
+
+    int cnt = 5;
+    while (cnt)
+    {
+        std::cout << "我是一个main线程, 我名字是main thread" << " 我的id: " << FormatId(pthread_self()) << std::endl;
+        sleep(1);
+        cnt--;
+    }
+
+    pthread_join(tid, nullptr);
+    return 0;
+}
