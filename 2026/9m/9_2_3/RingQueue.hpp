8c88604e77389024fde8bb71e1e21d1c11bbe53e
HCYshawn <1754099901@qq.com>
2026-09-02 19:24:29 +0800

    多生产单消费循环队列模拟

---
+++ 2026/9m/9_2_3/RingQueue.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "Sem.hpp"
+#include "Mutex.hpp"
+
+static const int gcap = 5;
+
+using namespace SemModule;
+using namespace MutexModule;
+
+template <typename T>
+class RingQueue
+{
+public:
+    RingQueue(int cap = gcap)
+        : _cap(cap),
+          _rq(cap),
+          _blank_sem(cap),
+          _p_step(0),
+          _data_sem(0),
+          _c_step(0)
+    {
+    }
+
+    void Equeue(const T &in)
+    {
+        // 生产者
+
+        // 1. 申请信号量，即空位置信号量
+        _blank_sem.P();
+
+        {
+            //_pmutex.Lock();
+            LockGuard lockguard(_pmutex);
+            // 2. 生产
+            _rq[_p_step] = in;
+            // 3. 更新下标
+            ++_p_step;
+            // 4. 维持环形特性
+            _p_step %= _cap;
+
+            //_pmutex.Unlock();
+        }
+        _data_sem.V();
+    }
+
+    void Pop(T *out)
+    {
+
+        // 消费者
+        // 1. 申请信号量，即数据信号量
+        _data_sem.P();
+        {
+            LockGuard lockguard(_cmutex);
+            //_cmutex.Lock();
+
+            // 2. 生产
+            *out = _rq[_c_step];
+            // 3. 更新下标
+            ++_c_step;
+            // 4. 维持环形特性
+            _c_step %= _cap;
+
+            //_cmutex.Unlock();
+        }
+        _blank_sem.V();
+    }
+
+    ~RingQueue()
+    {
+    }
+
+private:
+    std::vector<T> _rq;
+    int _cap;
+
+    // 生产者
+    Sem _blank_sem;
+    int _p_step;
+
+    // 消费者
+    Sem _data_sem;
+    int _c_step;
+
+    // 维护多生产多消费，两把锁
+    Mutex _cmutex;
+    Mutex _pmutex;
+};
---
+++ 2026/9m/9_2_3/main.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <pthread.h>
+#include <ctime>
+#include <unistd.h>
+#include "RingQueue.hpp"
+
+void *consumer(void *args)
+{
+    RingQueue<int> *rq = static_cast<RingQueue<int> *>(args);
+    while (true)
+    {
+        sleep(3);
+        int t = 0;
+        rq->Pop(&t);
+
+        std::cout << "消费者拿到了一个数据: " << t << std::endl;
+    }
+}
+
+void *productor(void *args)
+{
+    RingQueue<int> *rq = static_cast<RingQueue<int> *>(args);
+    int data = 1;
+    while (true)
+    {
+
+        std::cout << "生产了一个任务: " << data << std::endl;
+
+        rq->Equeue(data);
+
+        data++;
+    }
+}
+
+int main()
+{
+    // 申请阻塞队列
+    RingQueue<int> *rq = new RingQueue<int>();
+
+    pthread_t c[2], p[3];
+    pthread_create(c, nullptr, consumer, rq);
+    pthread_create(c + 1, nullptr, consumer, rq);
+    pthread_create(p, nullptr, productor, rq);
+    pthread_create(p + 1, nullptr, productor, rq);
+    pthread_create(p + 2, nullptr, productor, rq);
+
+    pthread_join(c[0], nullptr);
+    pthread_join(c[1], nullptr);
+    pthread_join(p[0], nullptr);
+    pthread_join(p[1], nullptr);
+    pthread_join(p[2], nullptr);
+    return 0;
+}
